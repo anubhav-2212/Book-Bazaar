414e1dd97e31da282a3d6d998943792de9af1f7c
Anubhav Srivastva <srivastvaanubhav.2212@gmail.com>
2025-09-02 23:45:44 +0530

    addOrder error fixed

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -1,1 +1,35 @@
-const createOrder = async (req, res) => {}
+export const addOrder = async (req, res) => {
+    try {
+        const{shippingInfo,orderItems,paymentInfo,totalAmount}=req.body
+    if(!shippingInfo||!orderItems||!paymentInfo||!totalAmount){
+        return res.status(401).json({
+            success:false,
+            message:"Credentails Missing"
+        })
+    }
+    const newOrder=await new Order({
+        shippingInfo,
+        orderItems,
+        paymentInfo,
+        totalAmount
+    })
+    await newOrder.save()
+    res.status(201).json({
+        success:true,
+        message:"Order Added Successfully",
+        data:newOrder
+    })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success:false,
+            message:"Internal Server Error",
+            error
+        })
+        
+    }
+    
+}
+export const getUserOrder=async(req,res)=>{}
+export const getOrdersById=async(req,res)=>{}

99fad16558c42615a4d79b0a4a3e1be5556fbac4
Anubhav Srivastva <srivastvaanubhav.2212@gmail.com>
2025-09-03 13:03:15 +0530

    cart-items models  added

---
+++ server/models/cart-items.models.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const cartItemsSchema=new mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    bookId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Book",
+        required:true
+    },
+    quantity:{
+        type:Number,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    
+},{
+    timestamps:true
+})

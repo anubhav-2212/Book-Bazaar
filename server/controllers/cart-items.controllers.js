da0577bcaf2151fc2982858c607caa59f49bf098
Anubhav Srivastva <srivastvaanubhav.2212@gmail.com>
2025-09-05 23:43:00 +0530

    AuthMiddleware added in cart routes

--- server/controllers/cart-items.controllers.js
+++ server/controllers/cart-items.controllers.js
@@ -1,7 +1,28 @@
-
+import Cart from "../models/cart-items.models.js";
+import Books from "../models/books.models.js";
 
 export const addCartItem=async(req,res)=>{
-    
+    const {userId}=req.user
+    console.log(useId);
+    const{bookId,quantity,price}=req.body;
+    if(!bookId||!quantity||!price){
+        return res.status(400).json({
+            success:false,
+            message:"Credentails Missing"
+        })
+    }
+    const newCartItem=await new Cart({
+        userId,
+        bookId,
+        quantity,
+        price
+    })
+    await newCartItem.save()
+    res.status(201).json({
+        success:true,
+        message:"Cart Item Added Successfully",
+        data:newCartItem
+    })
 
 }
 export const getCartItems=async(req,res)=>{}
---
+++ server/routes/cart-items.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { addCartItem, deleteCartItem, getCartItems } from "../controllers/cart-items.controllers.js";
+import { authMiddleware } from '../middlewares/auth.middlewares.js';
+
+
+const cartItemsRoutes=express.Router();
+
+cartItemsRoutes.post("/addcart",authMiddleware,addCartItem)
+cartItemsRoutes.get("/getcart",authMiddleware,getCartItems)
+cartItemsRoutes.get("/deletecart",authMiddleware,deleteCartItem)
+
+export default cartItemsRoutes;
